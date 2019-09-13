c9e8dbeecc0003269e6576e277e694080d0c2c9c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-09-13 17:21:37 +0900

    #42

--- lib/makoto/quote_lib.rb
+++ lib/makoto/quote_lib.rb
@@ -34,6 +34,7 @@ module Makoto
     def quotes(params = {})
       params[:priority] ||= @config['/quote/priority/min']
       params[:form] ||= ['剣崎真琴']
+      params[:keyword] ||= ''
       quotes = clone.keep_if{|v| keep?(v, params)}
       return quotes if params[:detail].present?
       return quotes.map{|v| v['quote']}.uniq
@@ -45,8 +46,9 @@ module Makoto
       return false if params[:emotion].nil? && (entry['emotion'] == 'bad')
       return false if entry['priority'] < params[:priority]
       return false unless params[:form].include?(entry['form'])
-      return false if params[:keyword] && !entry['quote'].include?(params[:keyword])
-      return true
+      return true if entry['quote'].include?(params[:keyword])
+      return true if entry['remark'].include?(params[:keyword])
+      return false
     end
 
     def delete
--- lib/makoto/responder/dental_responder.rb
+++ lib/makoto/responder/dental_responder.rb
@@ -1,7 +1,9 @@
 module Makoto
   class DentalResponder < Responder
     def executable?
-      return @params['content'].match(/(歯医者|虫歯|歯みがき|歯磨き|抜歯)/)
+      return @params['content'].match(
+        /(歯医者|虫歯|歯みがき|歯磨き|抜歯|歯槽膿漏|知覚過敏|歯周病)/,
+      )
     end
 
     def exec
