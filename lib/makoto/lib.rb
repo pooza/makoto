f0e02aa0b0438dd044453926604ecb2f2f75cdb7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-09-17 08:48:53 +0900

    #49

---
+++ lib/makoto/lib.rb
@@ -0,0 +1,47 @@
+module Makoto
+  class Lib < Array
+    def initialize
+      super
+      @logger = Logger.new
+      @config = Config.instance
+      @http = HTTP.new
+      refresh unless exist?
+      load
+    end
+
+    def exist?
+      return File.exist?(path)
+    end
+
+    def underscore_name
+      return self.class.to_s.split('::').last.sub(/Lib$/, '').underscore
+    end
+
+    def path
+      return File.join(Environment.dir, 'tmp/cache', "#{underscore_name}_lib")
+    end
+
+    def load
+      return unless exist?
+      clear
+      concat(Marshal.load(File.read(path)))
+    end
+
+    def refresh
+      File.write(path, Marshal.dump(fetch))
+      load
+    rescue => e
+      @logger.error(e)
+    end
+
+    alias create refresh
+
+    def delete
+      File.unlink(path) if exist?
+    end
+
+    def fetch
+      return @http.get(@config["/#{underscore_name}/url"]).parsed_response
+    end
+  end
+end
