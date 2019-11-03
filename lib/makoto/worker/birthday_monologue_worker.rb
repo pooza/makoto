5d624373aa4990aa9aec29dd78d99d22eaf5bb31
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-11-03 22:18:33 +0900

    #71

--- lib/makoto/worker/birthday_monologue_worker.rb
+++ lib/makoto/worker/birthday_monologue_worker.rb
@@ -2,6 +2,9 @@ module Makoto
   class BirthdayMonologueWorker < Worker
     def perform
       template = Template.new('birthday')
+      template[:greeting] = @config['/birthday/greeting'].sample
+      template[:message] = @config['/birthday/messages'].sample
+      template[:url] = @tracks.tracks(makoto: true, detail: true).sample['url']
       mastodon.toot(template.to_s)
     end
   end
