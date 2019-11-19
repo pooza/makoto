4132a3036d0412da0246e2bff0ecfa97c88993ea
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-11-20 08:57:20 +0900

    #90

--- lib/makoto.rb
+++ lib/makoto.rb
@@ -16,7 +16,7 @@ require 'sidekiq-scheduler'
 require 'ginseng'
 
 module Makoto
-  def self.configure_autoload
+  def self.autoload
     loader = Zeitwerk::Loader.new
     loader.inflector.inflect(
       'http' => 'HTTP',
@@ -25,7 +25,7 @@ module Makoto
     loader.setup
   end
 
-  def self.configure_sidekiq
+  def self.sidekiq
     Sidekiq.configure_client do |config|
       config.redis = {url: Config.instance['/sidekiq/redis/dsn']}
     end
@@ -35,5 +35,5 @@ module Makoto
   end
 end
 
-Makoto.configure_autoload
-Makoto.configure_sidekiq
+Makoto.autoload
+Makoto.sidekiq
