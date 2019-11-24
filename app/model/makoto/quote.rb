952020c8332165b4c41c2e553415b8ecbb359928
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-11-24 10:50:42 +0900

    #93

--- app/model/makoto/quote.rb
+++ app/model/makoto/quote.rb
@@ -6,7 +6,7 @@ module Makoto
       config = Config.instance
       quotes = Quote.dataset.where(
         exclude: false,
-        form_id: Form.first(name: (params[:form] || config['/quote/default_forms'])).id,
+        form_id: (params[:form] || config['/quote/default_forms']).map{|v| Form.first(name: v).id},
       )
       quotes = quotes.where{(params[:priority] || config['/quote/priority/min']) <= priority}
       quotes = quotes.where(exclude_respond: false) if params[:respond]
