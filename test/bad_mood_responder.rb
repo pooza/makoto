43b054043f44a9c9c0b6d1b2d588db2ba02be2ab
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-11-24 11:16:36 +0900

    #93

--- test/bad_mood_responder.rb
+++ test/bad_mood_responder.rb
@@ -2,16 +2,23 @@ module Makoto
   class BadMoodResponderTest < Test::Unit::TestCase
     def setup
       @responder = BadMoodResponder.new
+      @config = Config.instance
     end
 
     def test_exec
       return if Environment.ci?
-      @responder.params = {'content' => 'ネギトロ丼'}
+      @responder.params = {
+        'content' => 'ネギトロ丼',
+        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
+      }
       assert_false(@responder.executable?)
 
-      @responder.params = {'content' => 'ちんこ'}
+      @responder.params = {
+        'content' => 'ちんこ',
+        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
+      }
       assert(@responder.executable?)
-      assert(@responder.exec.present?)
+      assert(@responder.exec.present?) unless Environment.ci?
     end
   end
 end
