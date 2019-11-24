2701f2a893d468a79535cbe67771aae22025fc8f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-11-25 01:25:56 +0900

    #87

--- app/task/test.rb
+++ app/task/test.rb
@@ -3,7 +3,7 @@ task :test do
   ENV['TEST'] = Makoto::Package.name
   require 'test/unit'
   require 'sidekiq/testing'
-  Makoto::Postgres.connect unless Makoto::Environment.ci?
+  Makoto::Postgres.connect
   Sidekiq::Testing.fake!
   Dir.glob(File.join(Makoto::Environment.dir, 'test/*.rb')).each do |t|
     require t
