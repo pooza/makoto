8d04c53aa12f47d601f37971ef839ca7d4b18c1b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-27 06:14:41 +0900

    #119

--- test/greeting_responder.rb
+++ test/greeting_responder.rb
@@ -3,47 +3,29 @@ module Makoto
     def setup
       @responder = GreetingResponder.new
       @config = Config.instance
+      @account = {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']}
     end
 
     def test_exec
-      @responder.params = {
-        'content' => '博多ラーメン！',
-        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
-      }
+      @responder.params = {'content' => '博多ラーメン！', 'account' => @account}
       assert_false(@responder.executable?)
 
+      @responder.params = {'content' => 'おはよう！', 'account' => @account}
       Timecop.travel(Time.parse('8:00'))
-      @responder.params = {
-        'content' => 'おはよう！',
-        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
-      }
       assert(@responder.executable?)
       assert(@responder.on_time?)
       assert(@responder.exec.present?)
-
       Timecop.travel(Time.parse('17:00'))
-      @responder.params = {
-        'content' => 'おはよう！',
-        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
-      }
       assert(@responder.executable?)
       assert_false(@responder.on_time?)
       assert(@responder.exec.present?)
 
+      @responder.params = {'content' => 'あけおめ！', 'account' => @account}
       Timecop.travel(Time.parse('2000/1/1'))
-      @responder.params = {
-        'content' => 'あけおめ！',
-        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
-      }
       assert(@responder.executable?)
       assert(@responder.on_time?)
       assert(@responder.exec.present?)
-
       Timecop.travel(Time.parse('2000/1/8'))
-      @responder.params = {
-        'content' => 'あけおめ！',
-        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
-      }
       assert(@responder.executable?)
       assert_false(@responder.on_time?)
       assert(@responder.exec.present?)
