3a38e02a9070f616f5c327fa52c80a4d7fa6b49f
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-27 07:12:08 +0900

    #120

--- test/good_morning_monologue_worker.rb
+++ test/good_morning_monologue_worker.rb
@@ -4,8 +4,20 @@ module Makoto
       @worker = GoodMorningMonologueWorker.new
     end
 
+    def test_holiday_greeting
+      Timecop.travel(Time.parse('2000/5/17'))
+      assert_nil(@worker.holiday_greeting)
+      Timecop.travel(Time.parse('2000/1/1'))
+      assert(@worker.holiday_greeting.present?)
+    end
+
+    def test_topic
+      assert(@worker.topic.present?)
+    end
+
     def test_perform
       return if Environment.ci?
+      Timecop.travel(Time.parse('2000/5/17'))
       @worker.perform
     end
   end
