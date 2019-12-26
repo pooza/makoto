465e514805bd5012b244305b9d26118abf088b30
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-12-27 06:05:03 +0900

    #119

--- test/greeting_responder.rb
+++ test/greeting_responder.rb
@@ -12,12 +12,40 @@ module Makoto
       }
       assert_false(@responder.executable?)
 
-      return if Environment.ci?
+      Timecop.travel(Time.parse('8:00'))
       @responder.params = {
         'content' => 'おはよう！',
         'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
       }
       assert(@responder.executable?)
+      assert(@responder.on_time?)
+      assert(@responder.exec.present?)
+
+      Timecop.travel(Time.parse('17:00'))
+      @responder.params = {
+        'content' => 'おはよう！',
+        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
+      }
+      assert(@responder.executable?)
+      assert_false(@responder.on_time?)
+      assert(@responder.exec.present?)
+
+      Timecop.travel(Time.parse('2000/1/1'))
+      @responder.params = {
+        'content' => 'あけおめ！',
+        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
+      }
+      assert(@responder.executable?)
+      assert(@responder.on_time?)
+      assert(@responder.exec.present?)
+
+      Timecop.travel(Time.parse('2000/1/8'))
+      @responder.params = {
+        'content' => 'あけおめ！',
+        'account' => {'display_name' => 'ぷーざ', 'acct' => @config['/test/acct']},
+      }
+      assert(@responder.executable?)
+      assert_false(@responder.on_time?)
       assert(@responder.exec.present?)
     end
   end
