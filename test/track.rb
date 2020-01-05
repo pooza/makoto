c08705dc7994d210add1b8d004ed69c82d5d8ac3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-05 23:36:00 +0900

    #128

--- test/track.rb
+++ test/track.rb
@@ -2,8 +2,8 @@ module Makoto
   class TrackTest < Test::Unit::TestCase
     def test_pickup
       track = Track.pickup
-      assert(track.is_a?(Track))
-      assert(track.title.is_a?(String))
+      assert_kind_of(Track, track)
+      assert_kind_of(String, track.title)
       assert(track.title.present?)
     end
   end
