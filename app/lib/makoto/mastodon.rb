00391b637b1f2db63e9435c6f54a34eaaca2f867
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-02-13 11:15:58 +0900

    .

--- app/lib/makoto/mastodon.rb
+++ app/lib/makoto/mastodon.rb
@@ -1,34 +1,5 @@
 module Makoto
   class Mastodon < Ginseng::Mastodon
     include Package
-
-    def initialize(uri = nil, token = nil)
-      super(uri, token)
-      @config = Config.instance
-    end
-
-    def followers(params = {})
-      headers = params[:headers] || {}
-      headers['Authorization'] ||= "Bearer #{@token}"
-      headers['X-Mulukhiya'] = package_class.full_name unless mulukhiya_enable?
-      id = params[:id] || @config['/mastodon/account/id']
-      return @http.get(create_uri("/api/v1/accounts/#{id}/followers"), {
-        body: '{}',
-        headers: headers,
-        limit: @config['/mastodon/account/limit'],
-      })
-    end
-
-    def followees(params = {})
-      headers = params[:headers] || {}
-      headers['Authorization'] ||= "Bearer #{@token}"
-      headers['X-Mulukhiya'] = package_class.full_name unless mulukhiya_enable?
-      id = params[:id] || @config['/mastodon/account/id']
-      return @http.get(create_uri("/api/v1/accounts/#{id}/following"), {
-        body: '{}',
-        headers: headers,
-        limit: @config['/mastodon/account/limit'],
-      })
-    end
   end
 end
