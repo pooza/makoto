12947c737ff043a9cc09263d9166acd66356b15e
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-02-26 05:41:26 +0900

    #156

--- app/model/makoto/series.rb
+++ app/model/makoto/series.rb
@@ -2,6 +2,8 @@ require 'unicode'
 
 module Makoto
   class Series < Sequel::Model(:series)
+    one_to_many :quote
+
     def self.get(name)
       name = Unicode.nfkc(name)
       unless series = Series.first(name: name)
