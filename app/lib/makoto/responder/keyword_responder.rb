38088a068c3e3959614e9ded8043a61b83ec1290
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-05-01 11:57:25 +0900

    #238

--- app/lib/makoto/responder/keyword_responder.rb
+++ app/lib/makoto/responder/keyword_responder.rb
@@ -33,12 +33,12 @@ module Makoto
     def create_word_entries
       words = analyzer.result.select {|v| v[:feature].present?}
       if account&.past_keyword.present?
-        rand(0..1).times do
+        rand(0..3).times do
           word = account.past_keyword.select {|v| v[:feature].present?}.sample
           words.push(word.values) if word
         end
       end
-      return words.shuffle
+      return words.uniq.shuffle
     end
   end
 end
