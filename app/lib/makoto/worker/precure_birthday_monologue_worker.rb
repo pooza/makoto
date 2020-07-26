7f619b02960cca16cc3a56ae0de7c974d08f8e03
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-26 18:56:39 +0900

    #315

--- app/lib/makoto/worker/precure_birthday_monologue_worker.rb
+++ app/lib/makoto/worker/precure_birthday_monologue_worker.rb
@@ -4,7 +4,7 @@ module Makoto
   class PrecureBirthdayMonologueWorker < Worker
     def perform
       girls = Precure.all.select(&:birthday?)
-      return unless girls.present?
+      return if girls.empty?
       template = Template.new('precure_birthday')
       template[:girls] = girls
       mastodon.toot(template.to_s)
