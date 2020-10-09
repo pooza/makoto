54a5745658aa19f92d7753ef11e51875a3737db3
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-10-09 16:54:08 +0900

    #346

--- app/lib/makoto/worker/makoto_birthday_monologue_worker.rb
+++ app/lib/makoto/worker/makoto_birthday_monologue_worker.rb
@@ -2,12 +2,17 @@ module Makoto
   class MakotoBirthdayMonologueWorker < Worker
     def perform
       template = Template.new('makoto_birthday')
-      template[:greeting] = @config['/birthday/greeting'].sample
-      template[:message] = @config['/birthday/messages'].sample
+      template[:greeting] = greetings.sample
+      template[:titlecall1] = @config['/birthday/titlecalls1'].sample
+      template[:titlecall2] = @config['/birthday/titlecalls2'].sample
       track = Track.pickup(makoto: true)
       template[:url] = track.url
       template[:intro] = track.intro
       mastodon.toot(status: template.to_s, visibility: visibility)
+    end
+
+    def greetings
+      return Message.dataset.where(type: 'birthday').all.map(&:message)
     end
   end
 end
