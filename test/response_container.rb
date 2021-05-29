a13dfb4aecd05d9b73a60516fc4089279834f219
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-05-30 08:08:06 +0900

    #565

--- test/response_container.rb
+++ test/response_container.rb
@@ -28,7 +28,7 @@ module Makoto
       @container.greetings.push('こんにちは。')
       @container.paragraphs.push('天ぷらそば')
       @container.paragraphs.push('ネギトロ丼')
-      assert_equal(@container.to_s, "こんにちは。\nネギトロ丼天ぷらそば")
+      assert_equal(@container.to_s, "こんにちは。\n天ぷらそばネギトロ丼")
     end
   end
 end
