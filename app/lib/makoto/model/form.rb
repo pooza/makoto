fe8aeb888dbd14110ad9cc61bb25fabfcc8e17c2
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-06-06 10:39:51 +0900

    #578

--- app/lib/makoto/model/form.rb
+++ app/lib/makoto/model/form.rb
@@ -1,6 +1,13 @@
 module Makoto
   class Form < Sequel::Model(:form)
+    include Package
     one_to_many :quote
+
+    def self.ids(names = nil)
+      return (names || config['/quote/default_forms']).map do |name|
+        Form.first(name: name)
+      end.reject(&:nil?).map(&:id)
+    end
 
     def self.get(name)
       return Form.first(name: name.nfkc) || Form.create(name: name.nfkc)
