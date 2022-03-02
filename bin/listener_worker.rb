43ee7a1ce36236260c3f1c546f22a734249c2328
pooza <tkoishi@b-shock.co.jp>
2022-03-02 09:40:19 +0900

    #750

--- bin/listener_worker.rb
+++ bin/listener_worker.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 $LOAD_PATH.unshift(File.join(File.expand_path('..', __dir__), 'app/lib'))
+ENV['RAKE'] = nil
 
 require 'makoto'
-module Makoto
-  ENV['RACK_ENV'] ||= Environment.type
-  MakotoListener.start
-end
+Makoto::MakotoListener.start
